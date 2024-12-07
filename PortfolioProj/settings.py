ce4417e136e8320df9fa9bbaeabd1d1003593a49
vrb323 <vrb323@lehigh.edu>
2024-12-06 19:20:14 -0500

    Update settings.py

--- PortfolioProj/settings.py
+++ PortfolioProj/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'PortfolioProj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Templates/jobs'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
