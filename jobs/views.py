c133740ad471584abfac0bed7fbcb5a7e0ab0b64
vrb323 <vrb323@lehigh.edu>
2024-12-07 17:35:38 -0500

    Update views.py

--- jobs/views.py
+++ jobs/views.py
@@ -4,8 +4,8 @@ from .models import Job
 # Create your views here.
 def home(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs': jobs})
+    return render(request, 'jobs/home.html',{'jobs': jobs})
 
 def detail(request, job_id):
     job_detail = get_object_or_404(Job, pk=job_id)
-    return render(request, 'jobs/detail.html', {'job': job_detail})
+    return render(request, 'jobs/detail.html',{'job': job_detail})
